63a2194d8808773ca068fb4240861d0bc5f251b3
Kevin Dulanto <kevin.dulanto@utec.edu.pe>
2024-11-15 20:32:50 -0500

    Update files

--- src/find_max_triple_product.h
+++ src/find_max_triple_product.h
@@ -1,10 +1,28 @@
-//
-// Created by rudri on 10/11/2020.
-//
-
 #ifndef PROG3_SIMULACRO_PC2_V2024_1_FIND_MAX_TRIPLE_PRODUCT_H
 #define PROG3_SIMULACRO_PC2_V2024_1_FIND_MAX_TRIPLE_PRODUCT_H
 
+#include <iterator>
+#include <unordered_set>
+#include <algorithm>
 
+using namespace std;
+
+template <typename ForwardIt>
+ForwardIt remove_duplicates(ForwardIt first, ForwardIt last) {
+    unordered_set<typename iterator_traits<ForwardIt>::value_type> seen;
+    return remove_if(first, last, [&seen](const auto& value) {
+        return !seen.insert(value).second;
+    });
+}
+
+template <typename ForwardIt, typename BinaryPredicate>
+ForwardIt remove_duplicates_if(ForwardIt first, ForwardIt last, BinaryPredicate pred) {
+    for (ForwardIt it = first; it != last; ++it) {
+        last = remove_if(next(it), last, [&](const auto& value) {
+            return pred(*it, value);
+        });
+    }
+    return last;
+}
 
 #endif //PROG3_SIMULACRO_PC2_V2024_1_FIND_MAX_TRIPLE_PRODUCT_H
--- src/remove_duplicates.h
+++ src/remove_duplicates.h
@@ -1,10 +1,52 @@
-//
-// Created by rudri on 10/11/2020.
-//
-
 #ifndef PROG3_SIMULACRO_PC2_V2024_1_REMOVE_DUPLICATES_H
 #define PROG3_SIMULACRO_PC2_V2024_1_REMOVE_DUPLICATES_H
 
+#include <iterator>
+#include <algorithm>
+#include <list>
+#include <numeric>
+#include <vector>
+#include <limits>
+#include <thread>
+#include <future>
 
+using namespace std;
+
+template <typename T>
+T product_of_all(const T& a, const T& b, const T& c) {
+    return a * b * c;
+}
+
+template <typename Container>
+auto find_max_triple_product(const Container& container) -> typename Container::value_type {
+    using ValueType = typename Container::value_type;
+
+    size_t n = container.size();
+    if (n <= 3) {
+        return accumulate(container.begin(), container.end(), static_cast<ValueType>(1), multiplies<>());
+    }
+
+    vector<ValueType> vec(container.begin(), container.end());
+
+    sort(vec.begin(), vec.end());
+
+    ValueType max_product = max(
+            vec[n - 1] * vec[n - 2] * vec[n - 3],
+            vec[0] * vec[1] * vec[n - 1]
+    );
+
+    return max_product;
+}
+
+template <typename T>
+auto find_max_triple_product(const list<T>& container) -> T {
+    size_t n = container.size();
+    if (n <= 3) {
+        return accumulate(container.begin(), container.end(), static_cast<T>(1), multiplies<>());
+    }
+
+    vector<T> vec(container.begin(), container.end());
+    return find_max_triple_product(vec);
+}
 
 #endif //PROG3_SIMULACRO_PC2_V2024_1_REMOVE_DUPLICATES_H
